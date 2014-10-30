38c61af01de1d13173a8782e9f5e22febded72eb
Brandon Koepke <bdkoepke@gmail.com>
2014-10-30 21:00:34 +0000

    Pulled the System.Data.SQLite.dll from the upstream master.tar.gz since the nuget binary is always looking for the native interop dll. Also added src/External/logs.css since that is required in UI/content and it isnt being built by gulp anymore... Finally I updated build.sh to also build the gulp files

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
 #!/bin/sh
-xbuild src/NzbDrone.sln /p:Configuration=Mono /p:Platform=x86 /t:Build
+set -e
+
+nzbdrone_xbuild() {
+	xbuild src/NzbDrone.sln /p:Configuration=Mono /p:Platform=x86 /t:Build 
+	cp src/External/System.Data.SQLite.dll _output/
+}
+nzbdrone_gulp() {
+	gulp build
+	cp src/External/logs.css _output/UI/Content
+}
+cleanup_mdb() {
+	rm _output/*.mdb
+	rm _output/NzbDrone.Update/*.mdb
+}
+nzbdrone_chmod() {
+	chmod -x _output/*.dll
+	chmod -x _output/NzbDrone.Update/*.dll
+}
+nzbdrone_xbuild && nzbdrone_gulp && cleanup_mdb && nzbdrone_chmod
