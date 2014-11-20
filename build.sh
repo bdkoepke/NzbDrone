214a0e7251972081f9ae7987acb144cbd294fff3
Charlie Root <root@sonarr.swordfish.pw>
2014-11-20 23:07:00 +0000

    Moved to Newtonsoft.Json 6.0.6

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 nzbdrone_xbuild() {
 	xbuild src/NzbDrone.sln /p:Configuration=Mono /p:Platform=x86 /t:Build 
-	for dll in System.Data.SQLite.dll MediaInfoDotNet.dll.config Microsoft.AspNet.SignalR.Core.dll Microsoft.AspNet.SignalR..Owin.dll NzbDrone.Core.dll
+	for dll in System.Data.SQLite.dll MediaInfoDotNet.dll.config Microsoft.AspNet.SignalR.Core.dll Microsoft.AspNet.SignalR.Owin.dll NzbDrone.Core.dll
 	do
 		cp src/External/${dll} _output/
 	done
